7eac25fa5cd883745475101579f8290dd6049b17
Piyush Singanjude <103660846+Piyushsinganjude@users.noreply.github.com>
2023-02-09 19:54:05 +0530

    Added solution - LeetHub

--- Minimum Platforms - GFG/minimum-platforms.java
+++ Minimum Platforms - GFG/minimum-platforms.java
@@ -47,22 +47,22 @@ class Solution
     //railway station such that no train waits.
     static int findPlatform(int arr[], int dep[], int n)
     {
-        Arrays.sort(arr);
-        Arrays.sort(dep);
-        int count =0;
-        int ans =0;
-        int i =0,j=0;
-        while(i<n){
-            if(arr[i]<=dep[j]){
-                count++;
-                ans = Math.max(ans,count);
-                i++;
-            } else if(arr[i]>dep[j]){
-                count--;
-                j++;
-            }
-        }
-        return ans;
+      Arrays.sort(arr);
+      Arrays.sort(dep);
+      int count =0;
+      int ans = 0;
+      int i=0,j=0;
+      while(i<n){
+          if(arr[i]<=dep[j]){
+              count++;
+              ans = Math.max(ans,count);
+              i++;
+          }else if(arr[i]>dep[j]){
+              count--;
+              j++;
+          }
+      }
+      return ans;
         
     }
     
