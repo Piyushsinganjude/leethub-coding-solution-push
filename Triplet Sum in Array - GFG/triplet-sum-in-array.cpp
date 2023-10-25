5571efc67a6ee89a8dc6a40cb72c6ce0738b3b14
Piyush Singanjude <103660846+Piyushsinganjude@users.noreply.github.com>
2023-10-25 19:19:58 +0530

    Added solution - LeetHub

--- Triplet Sum in Array - GFG/triplet-sum-in-array.cpp
+++ Triplet Sum in Array - GFG/triplet-sum-in-array.cpp
@@ -11,20 +11,24 @@ class Solution{
     bool find3Numbers(int A[], int n, int X)
     {
         sort(A,A+n);
-        for(int i =0;i<n;i++){
-            int y = X-A[i];
-            int low = i+1 , high=n-1;
-            while(low<high){
-                if(A[low] + A[high] == y){
-                    return 1;
-                } else if(A[low] + A[high] >y){
-                    high--;
-                } else if(A[low] + A[high] <y){
-                    low++;
+        
+        for(int i=0;i<n-1;i++){
+            int j = i + 1;
+            int k = n-1;
+            
+            while(j<k){
+                int sum = A[i] + A[j] + A[k];
+                if(sum > X){
+                    k--;
+                }
+                else if(sum < X){
+                    j++;
+                }else {
+                    return true;
                 }
             }
         }
-        return 0;
+        return false;
     }
 
 };
