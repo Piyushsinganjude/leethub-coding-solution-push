90647d14e6205bb77dac4b1bec8cc7b7c0197931
Piyush Singanjude <103660846+Piyushsinganjude@users.noreply.github.com>
2023-10-26 18:32:21 +0530

    Added solution - LeetHub

--- Zero Sum Subarrays - GFG/zero-sum-subarrays.cpp
+++ Zero Sum Subarrays - GFG/zero-sum-subarrays.cpp
@@ -12,19 +12,22 @@ class Solution{
 public:
     //Function to count subarrays with sum equal to 0.
     long long int findSubarray(vector<long long int> &arr, int n ) {
-        unordered_map<long long int, int> mp;
-        long long int sum = 0;
-        long long int ans = 0;
+        unordered_map<long long ,int> mp;
+        
+        long long sum = 0;
+        long long ans = 0;
         mp[0] = 1;
-        for(int i =0;i<n;i++)
-        {
+        
+        for(int i =0;i<n;i++){
             sum += arr[i];
-            if(mp.find(sum)!=mp.end()){
-                ans += mp[sum];
+            if(mp.find(sum)!= mp.end()){
+                ans +=mp[sum];
             }
             mp[sum]++;
         }
         return ans;
+        
+        
     }
 };
 
